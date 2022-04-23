e8f458b87037f88072e6d41c4e4e29f99aad2801
ali jafari <104249338+alisj9070@users.noreply.github.com>
2022-04-23 15:51:29 +0430

    deleted repasitory

--- src/Redux/actions/actionModal.js
+++
@@ -1,7 +0,0 @@
-export const modalAction=()=>{
-    return{type:'MODAL'}
-}
-export const modalProductAction=(id)=>(dispatch,getState)=>{
-    const findedModalProduct=getState().productList.find(product=>{return product.id===id})
-    dispatch({type:'MODAL_PRODUCT',payload:findedModalProduct})
-   }
